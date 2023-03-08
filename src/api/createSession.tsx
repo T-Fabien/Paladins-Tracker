ff4440f47497978c67ddb59978526b0f8eee30de
fabien <fabienthiot@gmail.com>
2023-03-08 19:41:43 +0100

    Hosting config

--- src/api/createSession.tsx
+++ src/api/createSession.tsx
@@ -2,6 +2,8 @@ export const createSession = async () => {
   // Request
   return fetch(`/api/createsession`).then((response) => {
     try {
+      console.log(response);
+      
       // If Server Error
       if (response.status >= 400) {
         return response.text().then(() => {
